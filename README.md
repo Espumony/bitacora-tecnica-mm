98cc2babb945e8960ec1dbce36565053da5e1145
Espumony <edpenag@gmail.com>
2025-07-13 19:13:43 -0600

    Primer commit: estructura inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bitácora Técnica MM
+
+Primer proyecto de Medical Mecánica.
+Este programa permite llevar el registro de mantenimientos técnicos a equipos médicos.
+
+Proyecto creado para aprender, desarrollar y contruir una herramienta funcional.
---
+++ bitacora/core.py
@@ -0,0 +1,13 @@
+#bitacora/core.py
+
+def agregar_entrada():
+    """Agrega una nueva entrada a la bitácora."""
+    pass
+
+def listar_entradas():
+    """Muestra todas las entrAdas registradas."""
+    pass
+
+def exportar_entradas():
+    """Exporta las entradas a un archivo externo."""
+    pass
